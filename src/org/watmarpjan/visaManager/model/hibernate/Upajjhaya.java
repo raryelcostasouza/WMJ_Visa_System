4ce28226c218bebddd8a8b6a70278ed9c5365bbf
WMJ_user <WMJ_user@WMJ50-Dell2330>
2016-09-27 17:07:51 +0700

    Renamed name field to upajjhaya_name

--- src/org/watmarpjan/visaManager/model/hibernate/Upajjhaya.java
+++ src/org/watmarpjan/visaManager/model/hibernate/Upajjhaya.java
@@ -6,8 +6,8 @@
 package org.watmarpjan.visaManager.model.hibernate;
 
 import java.io.Serializable;
-import java.util.Set;
 import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,9 +16,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
@@ -30,7 +28,7 @@ import javax.xml.bind.annotation.XmlTransient;
 {
     @NamedQuery(name = "Upajjhaya.findAll", query = "SELECT u FROM Upajjhaya u"),
     @NamedQuery(name = "Upajjhaya.findByIdupajjhaya", query = "SELECT u FROM Upajjhaya u WHERE u.idupajjhaya = :idupajjhaya"),
-    @NamedQuery(name = "Upajjhaya.findByName", query = "SELECT u FROM Upajjhaya u WHERE u.name = :name")
+    @NamedQuery(name = "Upajjhaya.findByUpajjhayaName", query = "SELECT u FROM Upajjhaya u WHERE u.upajjhayaName = :upajjhayaName")
 })
 public class Upajjhaya implements Serializable
 {
@@ -40,10 +38,9 @@ public class Upajjhaya implements Serializable
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     private Integer idupajjhaya;
-    private String name;
-    @OneToMany(mappedBy = "upajjhaya")
-    private Set<Profile> profileSet;
-    @JoinColumn(name = "monastery", referencedColumnName = "id")
+    @Column(name = "UPAJJHAYA_NAME")
+    private String upajjhayaName;
+    @JoinColumn(name = "MONASTERY", referencedColumnName = "ID")
     @ManyToOne
     private Monastery monastery;
 
@@ -66,25 +63,14 @@ public class Upajjhaya implements Serializable
         this.idupajjhaya = idupajjhaya;
     }
 
-    public String getName()
+    public String getUpajjhayaName()
     {
-        return name;
+        return upajjhayaName;
     }
 
-    public void setName(String name)
+    public void setUpajjhayaName(String upajjhayaName)
     {
-        this.name = name;
-    }
-
-    @XmlTransient
-    public Set<Profile> getProfileSet()
-    {
-        return profileSet;
-    }
-
-    public void setProfileSet(Set<Profile> profileSet)
-    {
-        this.profileSet = profileSet;
+        this.upajjhayaName = upajjhayaName;
     }
 
     public Monastery getMonastery()
