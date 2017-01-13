f90db73df300920ef7feee22a5bca872b9e38ec7
WMJ_user <WMJ_user@WMJ50-Dell2330>
2017-01-13 15:35:14 +0700

    *Bugfix - when clearing visa was not clearing associated visa extensions

--- src/org/watmarpjan/visaManager/control/CtrVisa.java
+++ src/org/watmarpjan/visaManager/control/CtrVisa.java
@@ -6,6 +6,7 @@
 package org.watmarpjan.visaManager.control;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import javax.persistence.Query;
 import javax.persistence.PersistenceException;
 import org.hibernate.exception.ConstraintViolationException;
@@ -20,12 +21,12 @@ import org.watmarpjan.visaManager.model.hibernate.VisaExtension;
  */
 public class CtrVisa extends AbstractControllerDB
 {
-    
+
     public CtrVisa(CtrDatabase ctrDB)
     {
         super(ctrDB);
     }
-    
+
     public int addVisaExt(VisaExtension vExt)
     {
         String errorMessage = "Unable to add visa extension.";
@@ -33,24 +34,26 @@ public class CtrVisa extends AbstractControllerDB
         {
             ctrDB.openTransaction();
             ctrDB.getSession().persist(vExt);
-           
+
             ctrDB.commitCurrentTransaction();
             return 0;
-            
-        } catch (PersistenceException hex)
+
+        }
+        catch (PersistenceException hex)
         {
             ctrDB.rollbackCurrentTransaction();
             if (hex instanceof ConstraintViolationException)
             {
                 CtrAlertDialog.databaseExceptionDialog((ConstraintViolationException) hex, errorMessage);
-            } else
+            }
+            else
             {
                 CtrAlertDialog.exceptionDialog(hex, errorMessage);
             }
             return -1;
         }
     }
-    
+
     public int removeVisaExt(VisaExtension vExt)
     {
         String errorMessage = "Unable to remove visa extension.";
@@ -60,139 +63,158 @@ public class CtrVisa extends AbstractControllerDB
             ctrDB.getSession().remove(vExt);
             ctrDB.commitCurrentTransaction();
             return 0;
-            
-        } catch (PersistenceException hex)
+
+        }
+        catch (PersistenceException hex)
         {
             ctrDB.rollbackCurrentTransaction();
             if (hex instanceof ConstraintViolationException)
             {
                 CtrAlertDialog.databaseExceptionDialog((ConstraintViolationException) hex, errorMessage);
-            } else
+            }
+            else
             {
                 CtrAlertDialog.exceptionDialog(hex, errorMessage);
             }
             return -1;
         }
     }
-    
+
     public VisaExtension loadVisaExtensionByNumber(String extNumber)
     {
         Query result;
         String hql;
-        
+
         hql = "from VisaExtension vext"
                 + " where vext.extNumber = '" + extNumber + "'";
-        
+
         result = ctrDB.getSession().createQuery(hql);
         return (VisaExtension) result.getSingleResult();
-        
+
     }
-    
+
     public ArrayList<EntryVisaExt> loadListExtensions(Integer idProfile)
     {
         Query result;
         String hql;
         ArrayList<EntryVisaExt> alVisaExtensions;
-        
+
         hql = "select new org.watmarpjan.visaManager.model.EntryVisaExt(vext.extNumber, vext.expiryDate)"
                 + " from MonasticProfile p"
                 + " inner join p.visaExtensionSet vext"
                 + " where p.idProfile = " + idProfile
                 + " order by vext.expiryDate desc";
-        
+
         result = ctrDB.getSession().createQuery(hql);
         alVisaExtensions = (ArrayList<EntryVisaExt>) result.getResultList();
-        
+
         return alVisaExtensions;
     }
-    
+
     private ArrayList<VisaExtension> loadListVisaExtForProfile(MonasticProfile p)
     {
         ArrayList<VisaExtension> listVisaExt;
         String hql;
         Query result;
-        
+
         hql = "from VisaExtension vext"
                 + " where vext.monasticProfile.idProfile = " + p.getIdProfile()
                 + " order by vext.expiryDate";
-        
+
         result = ctrDB.getSession().createQuery(hql);
         listVisaExt = (ArrayList<VisaExtension>) result.getResultList();
-        
+
         return listVisaExt;
     }
-    
+
     public VisaExtension getLastExtension(MonasticProfile p)
     {
         ArrayList<VisaExtension> listVisaExt;
         VisaExtension lastVext;
-        
+
         listVisaExt = loadListVisaExtForProfile(p);
         if (!listVisaExt.isEmpty())
         {
-            lastVext = listVisaExt.get(listVisaExt.size()-1);
+            lastVext = listVisaExt.get(listVisaExt.size() - 1);
         }
         else
         {
             lastVext = null;
         }
-        
+
         return lastVext;
-        
+
     }
-    
+
     public int addNewVisaForProfile(MonasticProfile p)
     {
         String errorMessage = "Unable to save the visa information.";
-        
+
         try
         {
             ctrDB.openTransaction();
             ctrDB.commitCurrentTransaction();
             return 0;
-            
-        } catch (PersistenceException hex)
+
+        }
+        catch (PersistenceException hex)
         {
             ctrDB.rollbackCurrentTransaction();
             if (hex instanceof ConstraintViolationException)
             {
                 CtrAlertDialog.databaseExceptionDialog((ConstraintViolationException) hex, errorMessage);
-            } else
+            }
+            else
             {
                 CtrAlertDialog.exceptionDialog(hex, errorMessage);
             }
             return -1;
         }
     }
-    
+
     public int clearVisaInfoForProfile(MonasticProfile p)
     {
-        String errorMessage = "Unable to clear visa extensions and passport scans.";
-        
+        String errorMessage = "Unable to clear visa/extensions data.";
+
         p.setVisaNumber(null);
         p.setVisaType(null);
         p.setVisaExpiryDate(null);
         p.setNext90DayNotice(null);
-        
+
         try
         {
             ctrDB.openTransaction();
-                     
+
+            //if there are any associated visa extensions
+            if (p.getVisaExtensionSet() != null)
+            {
+                //loop for clearing any associated visa extensions
+                for (Iterator<VisaExtension> it = p.getVisaExtensionSet().iterator(); it.hasNext();)
+                {
+                    VisaExtension objVisaExt = it.next();
+                    ctrDB.getSession().remove(objVisaExt);
+
+                }
+                p.getVisaExtensionSet().clear();
+            }
+
             ctrDB.commitCurrentTransaction();
             return 0;
-            
-        } catch (PersistenceException hex)
+
+        }
+        catch (PersistenceException hex)
         {
             ctrDB.rollbackCurrentTransaction();
             if (hex instanceof ConstraintViolationException)
             {
                 CtrAlertDialog.databaseExceptionDialog((ConstraintViolationException) hex, errorMessage);
-            } else
+            }
+            else
             {
                 CtrAlertDialog.exceptionDialog(hex, errorMessage);
             }
             return -1;
         }
     }
-    
+
 }
