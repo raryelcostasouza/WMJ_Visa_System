5e0b0dd8996166dbae7e06d9a9bd0a076770f385
WMJ_user <WMJ_user@WMJ50-Dell2330>
2020-02-16 20:40:49 +0700

    Added file for monastery class util functions

---
+++ src/org/watmarpjan/visaManager/util/MonasteryUtil.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.watmarpjan.visaManager.util;
+
+import org.watmarpjan.visaManager.model.hibernate.Monastery;
+
+/**
+ *
+ * @author adhipanyo
+ */
+public class MonasteryUtil
+{
+
+    public static String getStringWatAddrFull(Monastery m, boolean includeCountry, boolean includeTambon)
+    {
+        String sAddr = "";
+        
+        sAddr =  m.getMonasteryName();
+        if (m.getAddrCountry().equals("THAILAND"))
+        {
+            if (includeTambon)
+            {
+                sAddr += " ต." +m.getAddrTambon();
+            }
+            sAddr +=  " อ." + m.getAddrAmpher() + " จ." + m.getAddrJangwat();
+        }
+        else
+        {
+            if (includeTambon)
+            {
+                sAddr += " " +m.getAddrTambon();
+            }
+            
+            sAddr += m.getMonasteryName() + " " + m.getAddrAmpher() + " " + m.getAddrJangwat();
+            
+            if (includeCountry)
+            {
+                sAddr += " " + m.getAddrCountry();
+            } 
+        }
+        return sAddr;
+    }
+}
