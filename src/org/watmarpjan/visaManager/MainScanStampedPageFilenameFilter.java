de297595b3a61446f7450885aa897388868f4fef
WMJ_user <WMJ_user@WMJ50-Dell2330>
2020-02-19 20:53:54 +0700

    File filtering changes and class renaming

--- src/org/watmarpjan/visaManager/MainScanStampedPageFilenameFilter.java
+++ src/org/watmarpjan/visaManager/MainScanStampedPageFilenameFilter.java
@@ -12,14 +12,17 @@ import java.io.FilenameFilter;
  *
  * @author KroobaHariel
  */
-public class ExtraScanFileNameFilter implements FilenameFilter
+public class MainScanStampedPageFilenameFilter implements FilenameFilter
 {
 
     @Override
     public boolean accept(File dir, String name)
     {
         //if matches the filename regex format for the extra scans
-        return name.matches("(\\w)+(-page)[0-9]+-(\\S)*");
+        //they contain a final string with ArriveStamp, Visa or VisaExt String at the end
+        
+        //note that the string between the last dash and the extension CANNOT be EMPTY (\S stands for 1 or more ocurrences of non whitespace stuff)
+        return name.matches("(\\w)+(-page)[0-9]+-(\\S)+(.jpg)");
     }
     
 }
