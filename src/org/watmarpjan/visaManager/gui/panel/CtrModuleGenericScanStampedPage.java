a9d0904468acc3356038dc1017a9eb6488d84542
WMJ_user <WMJ_user@WMJ50-Dell2330>
2020-02-20 12:45:19 +0700

    Unnecessary functions removed. Panel Index number added

--- src/org/watmarpjan/visaManager/gui/panel/CtrModuleGenericScanStampedPage.java
+++ src/org/watmarpjan/visaManager/gui/panel/CtrModuleGenericScanStampedPage.java
@@ -14,8 +14,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import org.watmarpjan.visaManager.control.CtrFileOperation;
 import org.watmarpjan.visaManager.gui.util.GUIUtil;
 import org.watmarpjan.visaManager.model.eps.InfoPassportScanStampedPage;
 import org.watmarpjan.visaManager.model.hibernate.MonasticProfile;
@@ -39,6 +41,9 @@ public class CtrModuleGenericScanStampedPage
     private TextField tfRightPage; 
     
     @FXML
+    private TitledPane tpMain;
+    
+    @FXML
     private ImageView ivScan;
     
     private boolean scanContent;
@@ -60,11 +65,12 @@ public class CtrModuleGenericScanStampedPage
         tfRightPage = pTFRightPageNumber;
     }
     
-    public void init(CtrPanePassport pCtrPanePassport)
+    public void init(CtrPanePassport pCtrPanePassport, int index)
     {
         scanContent = false;
         objCtrPanePassport = pCtrPanePassport;
-        
+      
+        tpMain.setText("Stamped Page Scan " + index);
         tfLeftPage.textProperty().addListener(new ChangeListener<String>()
         {
             @Override
@@ -89,6 +95,12 @@ public class CtrModuleGenericScanStampedPage
         
         
         GUIUtil.loadImageView(ivScan, GUIUtil.IMG_TYPE_PASSPORT, null);
+    }
+    
+    @FXML
+    public void actionIMGClicked(MouseEvent me)
+    {
+        CtrFileOperation.openFileOnDefaultProgram(fScan);
     }
 
     public Button getbSelectFile()
@@ -175,28 +187,6 @@ public class CtrModuleGenericScanStampedPage
         
     }
     
-    private void fillDataContentScans(MonasticProfile p, boolean lockStatus)
-    {
-        
-        
-        
-          //reset();
-          
-//        fieldsScan2.reset();
-//        fieldsScan3.reset();
-//
-//        fScan1Selected = null;
-//        fScan2Selected = null;
-//        fScan3Selected = null;
-//
-//        refreshExtraPassportScan(p);
-//        toggleLockButtonsScanFirstPageNDepartureCard(lockStatus, p);
-//
-//        fillDataContentScanGeneric(fieldsScan1, objEPS1, lockStatus);
-//        fillDataContentScanGeneric(fieldsScan2, objEPS2, lockStatus);
-//        fillDataContentScanGeneric(fieldsScan3, objEPS3, lockStatus);
-    }
-    
     @FXML
     void actionArchiveExtraScan(ActionEvent ae)
     {
@@ -256,79 +246,5 @@ public class CtrModuleGenericScanStampedPage
 //        {
 //            CtrAlertDialog.errorDialog(ERROR_NO_PASSPORT_REGISTERED);
 //        }
-//    
     }
-    
-    @FXML
-    void actionIMGPassportScanClicked(MouseEvent me)
-    {
-//        MonasticProfile p;
-//        File fImgScan;
-//
-//        p = ctrGUIMain.getCtrPaneSelection().getSelectedProfile();
-//
-//        if (me.getSource().equals(ivPassportScan))
-//        {
-//            fImgScan = AppFiles.getScanPassportFirstPage(p.getNickname(), p.getPassportNumber());
-//        } else if (me.getSource().equals(ivDepartureCardScan))
-//        {
-//            fImgScan = AppFiles.getScanDepartureCard(p.getNickname());
-//        } else if (me.getSource().equals(ivScan1))
-//        {
-//            //if there is a scan selected, but not yet added
-//            if (fScan1Selected != null)
-//            {
-//                fImgScan = fScan1Selected;
-//            } //for a registered scan
-//            else
-//            {
-//                if (objEPS1 != null)
-//                {
-//                    fImgScan = objEPS1.getFileScan();
-//                } else
-//                {
-//                    fImgScan = null;
-//                }
-//            }
-//        } else if (me.getSource().equals(ivScan2))
-//        {
-//            //if there is a scan selected, but not yet added
-//            if (fScan2Selected != null)
-//            {
-//                fImgScan = fScan2Selected;
-//            } //for a registered scan
-//            else
-//            {
-//                if (objEPS2 != null)
-//                {
-//                    fImgScan = objEPS2.getFileScan();
-//                } else
-//                {
-//                    fImgScan = null;
-//                }
-//
-//            }
-//        } else
-//        {
-//            //if there is a scan selected, but not yet added
-//            if (fScan3Selected != null)
-//            {
-//                fImgScan = fScan3Selected;
-//            } //for a registered scan
-//            else
-//            {
-//                if (objEPS3 != null)
-//                {
-//                    fImgScan = objEPS3.getFileScan();
-//                } else
-//                {
-//                    fImgScan = null;
-//                }
-//
-//            }
-//        }
-//
-//        CtrFileOperation.openFileOnDefaultProgram(fImgScan);
-   }
-
 }
