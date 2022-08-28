52bfdf5b41f66ca9e73418ce7b694c9e798db0b5
WMJ_user <WMJ_user@WMJ50-Dell2330>
2022-08-28 16:24:03 -0300

    Version info update

--- src/org/watmarpjan/visaManager/Init.java
+++ src/org/watmarpjan/visaManager/Init.java
@@ -20,7 +20,7 @@ import org.watmarpjan.visaManager.gui.util.CtrAlertDialog;
 public class Init extends Application
 {
 
-    public static final String APP_VERSION = "v1.9.1 - 15/03/2020";
+    public static final String APP_VERSION = "v1.9.2 - 28/08/2022";
     public static Stage MAIN_STAGE;
     public static HostServices HOST_SERVICES;
     public static Application APP;
