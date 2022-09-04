8b49775fa6ca3286289f364e8451de462bcbd2e6
WMJ_user <WMJ_user@WMJ50-Dell2330>
2022-09-04 20:14:55 -0300

    Moved TM86 filling to specific classes

--- src/org/watmarpjan/visaManager/control/CtrPDF.java
+++ src/org/watmarpjan/visaManager/control/CtrPDF.java
@@ -342,91 +342,6 @@ public class CtrPDF
 
     }
 
-    public void fillFormTM86VisaChange(PDAcroForm acroForm, MonasticProfile p) throws IOException
-    {
-        ArrayList<PDTextField> alThaiFields;
-        Monastery mResidingAt;
-        LocalDate ldBirthDate, ldPassportIssue, ldPassportExpiry, ldLastEntry;
-        String immOffice;
-
-        alThaiFields = new ArrayList<>();
-        alThaiFields.add((PDTextField) acroForm.getField("immigrationOfficeThai"));
-        alThaiFields.add((PDTextField) acroForm.getField("titleThai"));
-        alThaiFields.add((PDTextField) acroForm.getField("watResidingAtThai"));
-        alThaiFields.add((PDTextField) acroForm.getField("addrNumberWatResidingAtThai"));
-        alThaiFields.add((PDTextField) acroForm.getField("addrRoadWatResidingAtThai"));
-        alThaiFields.add((PDTextField) acroForm.getField("addrTambonWatResidingAtThai"));
-        alThaiFields.add((PDTextField) acroForm.getField("addrAmpherWatResidingAtThai"));
-        alThaiFields.add((PDTextField) acroForm.getField("addrJangwatWatResidingAtThai"));
-        alThaiFields.add((PDTextField) acroForm.getField("visaType"));
-        adjustFontThaiField(alThaiFields);
-
-        acroForm.getField("titleThai").setValue(ProfileUtil.getTitleTH(p));
-        acroForm.getField("lastName").setValue(p.getLastName());
-        acroForm.getField("name").setValue(p.getMonasticName());
-        acroForm.getField("middleName").setValue(p.getMiddleName());
-
-        acroForm.getField("age").setValue(ProfileUtil.getStrAge(p.getBirthDate()));
-
-        ldBirthDate = Util.convertDateToLocalDate(p.getBirthDate());
-        if (ldBirthDate != null)
-        {
-            acroForm.getField("birthDateDay").setValue(ldBirthDate.getDayOfMonth() + "");
-            acroForm.getField("birthDateMonth").setValue(ldBirthDate.getMonthValue() + "");
-            acroForm.getField("birthDateYear").setValue(Util.convertYearToThai(ldBirthDate.getYear()) + "");
-        }
-
-        acroForm.getField("birthPlace_birthCountry").setValue(p.getBirthPlace() + ", " + p.getBirthCountry());
-        acroForm.getField("nationality").setValue(p.getNationality());
-        acroForm.getField("passportNumber").setValue(p.getPassportNumber());
-
-        ldPassportIssue = Util.convertDateToLocalDate(p.getPassportIssueDate());
-        if (ldPassportIssue != null)
-        {
-            acroForm.getField("passportIssueDateDay").setValue(ldPassportIssue.getDayOfMonth() + "");
-            acroForm.getField("passportIssueDateMonth").setValue(ldPassportIssue.getMonthValue() + "");
-            acroForm.getField("passportIssueDateYear").setValue(Util.convertYearToThai(ldPassportIssue.getYear()) + "");
-        }
-        acroForm.getField("passportIssuedAt").setValue(p.getPassportIssuedAt());
-
-        ldPassportExpiry = Util.convertDateToLocalDate(p.getPassportExpiryDate());
-        if (ldPassportExpiry != null)
-        {
-            acroForm.getField("passportExpiryDateDay").setValue(ldPassportExpiry.getDayOfMonth() + "");
-            acroForm.getField("passportExpiryDateMonth").setValue(ldPassportExpiry.getMonthValue() + "");
-            acroForm.getField("passportExpiryDateYear").setValue(Util.convertYearToThai(ldPassportExpiry.getYear()) + "");
-        }
-
-        acroForm.getField("visaType").setValue(p.getVisaType());
-        acroForm.getField("arrivalTravelBy").setValue(p.getArrivalTravelBy());
-        acroForm.getField("arrivalTravelFrom").setValue(p.getArrivalTravelFrom());
-        acroForm.getField("arrivalPortOfEntry").setValue(p.getArrivalPortOfEntry());
-
-        ldLastEntry = Util.convertDateToLocalDate(p.getArrivalLastEntryDate());
-        if (ldLastEntry != null)
-        {
-            acroForm.getField("arrivalLastEntryDateDay").setValue(ldLastEntry.getDayOfMonth() + "");
-            acroForm.getField("arrivalLastEntryDateMonth").setValue(ldLastEntry.getMonthValue() + "");
-            acroForm.getField("arrivalLastEntryDateYear").setValue(Util.convertYearToThai(ldLastEntry.getYear()) + "");
-        }
-        acroForm.getField("departureCardNumber").setValue(p.getArrivalCardNumber());
-        acroForm.getField("fullName").setValue(ProfileUtil.getFullName(p));
-        mResidingAt = p.getMonasteryResidingAt();
-        if (mResidingAt != null)
-        {
-             //immigration office string need to have the province of the residence monastery
-            immOffice = "ตรวจคนเข้าเมืองจังหวัด"+mResidingAt.getAddrJangwat();
-            acroForm.getField("immigrationOfficeThai").setValue(immOffice);
-        
-            acroForm.getField("watResidingAtThai").setValue(mResidingAt.getMonasteryName());
-            acroForm.getField("addrNumberWatResidingAtThai").setValue(mResidingAt.getAddrNumber());
-            acroForm.getField("addrRoadWatResidingAtThai").setValue(mResidingAt.getAddrRoad());
-            acroForm.getField("addrTambonWatResidingAtThai").setValue(mResidingAt.getAddrTambon());
-            acroForm.getField("addrAmpherWatResidingAtThai").setValue(mResidingAt.getAddrAmpher());
-            acroForm.getField("addrJangwatWatResidingAtThai").setValue(mResidingAt.getAddrJangwat());
-        }
-    }
-
     public void fillFormTM8Reentry(PDAcroForm acroForm, MonasticProfile p, boolean reentryTogetherExtension) throws IOException
     {
         ArrayList<PDTextField> alThaiFields;
@@ -556,7 +471,9 @@ public class CtrPDF
 
             // get the document catalog
             acroForm = pdfDocument.getDocumentCatalog().getAcroForm();
-
+            
+            //acroForm.setNeedAppearances(true);
+            
             //if the document is a PDF Form
             if (acroForm != null)
             {
@@ -582,10 +499,6 @@ public class CtrPDF
             else if (sourceFile.getName().equals(AppFiles.getFormTM8Reentry(monasteryNickname).getName()))
             {
                 fillFormTM8Reentry(acroForm, p, extraOption);
-            }
-            else if (sourceFile.getName().equals(AppFiles.getFormTM86VisaChange(monasteryNickname).getName()))
-            {
-                fillFormTM86VisaChange(acroForm, p);
             }
             else
             {
