aab57e9f840816be3c9683b007697b0f36f8e2d3
WMJ_user <WMJ_user@WMJ50-Dell2330>
2023-08-23 01:00:10 -0300

    commit files from previous release

---
+++ src/org/watmarpjan/visaManager/model/dueTask/TaskExtendNonImmVisaGeneric.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.watmarpjan.visaManager.model.dueTask;
+
+import java.time.LocalDate;
+import java.util.Date;
+import javafx.beans.property.SimpleStringProperty;
+import org.watmarpjan.visaManager.model.hibernate.Monastery;
+import org.watmarpjan.visaManager.util.Util;
+
+/**
+ *
+ * @author pm.dell
+ */
+public class TaskExtendNonImmVisaGeneric extends TaskExtendTouristVisaNotExtended
+{
+
+    private SimpleStringProperty prawat;
+    private SimpleStringProperty samnakput;
+
+    public TaskExtendNonImmVisaGeneric(String profileNickname, Date dDueDate, Monastery monasteryResidingAt, String passportKeptAt)
+    {
+        super(profileNickname, dDueDate, monasteryResidingAt, passportKeptAt);
+    }
+
+    public String getPrawat()
+    {
+        return prawat.get();
+    }
+
+    protected void setPrawat(LocalDate ldPrawat)
+    {
+        this.prawat = new SimpleStringProperty(ldPrawat.format(Util.DEFAULT_DATE_FORMAT));
+    }
+
+    public String getSamnakput()
+    {
+        return samnakput.get();
+    }
+
+    protected void setSamnakput(LocalDate ldSNP)
+    {
+        this.samnakput = new SimpleStringProperty(ldSNP.format(Util.DEFAULT_DATE_FORMAT));
+    }
+}
