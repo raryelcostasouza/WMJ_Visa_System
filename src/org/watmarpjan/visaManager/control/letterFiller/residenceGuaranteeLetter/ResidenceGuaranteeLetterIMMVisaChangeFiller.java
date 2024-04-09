3096cfa3fb58f257002af5932071e34013ed127c
WMJ_user <WMJ_user@WMJ50-Dell2330>
2024-04-09 20:46:16 -0300

    residence guarantee letter refactor

--- src/org/watmarpjan/visaManager/control/letterFiller/residenceGuaranteeLetter/ResidenceGuaranteeLetterIMMVisaChangeFiller.java
+++ src/org/watmarpjan/visaManager/control/letterFiller/residenceGuaranteeLetter/ResidenceGuaranteeLetterIMMVisaChangeFiller.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package org.watmarpjan.visaManager.control.letterFiller;
+package org.watmarpjan.visaManager.control.letterFiller.residenceGuaranteeLetter;
 
 import org.watmarpjan.visaManager.AppFiles;
 import org.watmarpjan.visaManager.model.hibernate.MonasticProfile;
---
+++ src/org/watmarpjan/visaManager/control/letterFiller/residenceGuaranteeLetter/ResidenceGuaranteeLetterSNPFiller.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.watmarpjan.visaManager.control.letterFiller.residenceGuaranteeLetter;
+
+import java.io.File;
+import org.odftoolkit.simple.common.navigation.InvalidNavigationException;
+import org.watmarpjan.visaManager.control.letterFiller.ODTLetterFiller;
+import org.watmarpjan.visaManager.model.hibernate.MonasticProfile;
+import org.watmarpjan.visaManager.util.ProfileUtil;
+
+/**
+ *
+ * @author raryel
+ */
+public abstract class ResidenceGuaranteeLetterSNPFiller extends ODTLetterFiller
+{
+    public ResidenceGuaranteeLetterSNPFiller(File fTemplate, MonasticProfile p)
+    {
+        super(fTemplate, p);
+    }
+
+    @Override
+    public void fillLetter() throws InvalidNavigationException, NullPointerException
+    {
+        fillField( "«titleTH2»");
+        
+        fillField( "«name»");
+        fillField( "«middleName»");
+        fillField( "«lastName»");
+        
+        fillField( "«paliNameThai»");
+        fillField( "«age»");
+        fillField( "«vassa»");
+        fillField( "«ethnicity»");
+        fillField( "«nationality»");
+        fillField( "«passportCountry»");
+        fillField( "«passportNumber»");
+        
+        fillField( "«WatResidingAtThai»");
+        
+        if (ProfileUtil.isOrdainedInThailand(monastic))
+        {
+            fillField( "«WatResidingAtThaiaddrTambon»");
+        }
+        else
+        {
+            fillField( "«WatResidingAtThaiaddrCountry»");
+        }
+        
+        fillField( "«WatResidingAtThaiaddrAmpher»");
+        fillField( "«WatResidingAtThaiaddrJangwat»");
+    }
+    
+}
