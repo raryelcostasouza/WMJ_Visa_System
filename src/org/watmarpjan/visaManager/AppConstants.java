54e054bff00d0963ee6f46e3dd617103bb284431
WMJ_user <WMJ_user@WMJ50-Dell2330>
2024-09-28 18:12:26 -0300

    Support for ผ.60 visa exemption

--- src/org/watmarpjan/visaManager/AppConstants.java
+++ src/org/watmarpjan/visaManager/AppConstants.java
@@ -38,12 +38,12 @@ public class AppConstants
     
     public static final String[] LIST_VISA_TYPES = new String[]
     {
-        "Privilege Entry", "ผ.15","ผ.30","ผผ.30","ผผ.90","Non-B","Non-O", "Non-RE", "Non-ED", VISA_TYPE_TOURIST
+        "Privilege Entry", "ผ.15","ผ.30","ผผ.30","ผ.60","ผผ.90","Non-B","Non-O", "Non-RE", "Non-ED", VISA_TYPE_TOURIST
     };
     
     public static final String[] LIST_VISA_EXEMPTIONS= new String[]
     {
-       "Privilege Entry","ผ.15","ผ.30","ผผ.30","ผผ.90"
+       "Privilege Entry","ผ.15","ผ.30","ผ.60","ผผ.30","ผผ.90"
     };
 
     public static final String[] LIST_TRAVEL_BY = new String[]
