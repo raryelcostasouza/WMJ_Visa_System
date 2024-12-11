adcc2a2f49cd081006437eb6c1373bb85e3d76b4
WMJ_user <WMJ_user@WMJ50-Dell2330>
2024-12-11 00:13:48 -0300

    bump beta version

--- src/org/watmarpjan/visaManager/Init.java
+++ src/org/watmarpjan/visaManager/Init.java
@@ -20,7 +20,7 @@ import org.watmarpjan.visaManager.gui.util.CtrAlertDialog;
 public class Init extends Application
 {
 
-    public static final String APP_VERSION = "v1.16 - 29/09/2024";
+    public static final String APP_VERSION = "v1.17 BETA_1 - 11/12/2024";
     public static Stage MAIN_STAGE;
     public static HostServices HOST_SERVICES;
     public static Application APP;
